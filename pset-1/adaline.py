74747ad50288bc720c88195df123613ed6953295
Kevin Meng <kevin.meng.01@gmail.com>
2022-02-22 21:09:59 -0500

    improve perceptron spaghetti code

--- pset-1/adaline.py
+++ pset-1/adaline.py
@@ -3,49 +3,42 @@ import matplotlib.pyplot as plt
 from scipy.ndimage.interpolation import rotate
 
 
-class Perceptron:
+class Adaline:
     """
     Convention:
     w is a weight matrix of shape 1 x d
     x is a data matrix of shape d x n
-    y is a target matrix of shape 1 x n
     """
 
     def __init__(self, d):
-        self.d = d
-        self.w = None
-
-        self.init_w()
-    
-    def init_w(self):
         np.random.seed(999)
-        self.w = np.random.rand(1, self.d)
+        self.d = d
+        self.w = np.random.rand((1, self.d))
 
     def predict(self, x):
-        y_hat = np.sign(self.w @ x)
-        y_hat[y_hat == 0] = 1
-        return y_hat
+        return self.w @ x
 
     def get_accuracy(self, x, y):
-        return np.mean(self.predict(x) == y)
+        y_hat = np.sign(self.w @ x).flatten()
+        return np.mean(y_hat == y)
 
     def train(self, x, y, pca, epochs=10, lr=0.1, plot_interval=1, plot_progress=True):
         """
         y should be a vector of {-1, 1}
         """
 
-        self.init_w()
+        np.random.seed(999)
+        self.w = np.random.rand(self.d)
 
         to_plot = []
         tot_epochs = 0
 
         for e in range(epochs):
             # Train
-            for i in range(x.shape[1]):
-                x_i, y_i = x[:, i:i + 1], y[:, i:i + 1]
+            for i, (x_i, y_i) in enumerate(zip(x, y)):
                 y_hat_i = self.predict(x_i)
                 if y_hat_i != y_i:
-                    self.w += lr * (-y_hat_i * x_i.T)
+                    self.w += lr * (-y_hat_i * x_i)
 
                 if i % plot_interval == 0:
                     to_plot.append(self.get_accuracy(x, y))
@@ -67,7 +60,7 @@ class Perceptron:
         theta = 90
         r = np.array(((np.cos(theta), -np.sin(theta)), (np.sin(theta), np.cos(theta))))
 
-        weight_normal = pca.transform(self.w)
+        weight_normal = pca.transform(np.expand_dims(self.w, axis=0))
         weight_parallel = (r @ weight_normal.T).squeeze()
 
         return weight_parallel
