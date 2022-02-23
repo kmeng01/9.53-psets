1aea0cbf13413473a8c0fca99d2c3b5db7cb281d
Kevin Meng <kevin.meng.01@gmail.com>
2022-02-22 21:26:50 -0500

    implement adaline

--- pset-1/perceptron.py
+++ pset-1/perceptron.py
@@ -11,23 +11,32 @@ class Perceptron:
     y is a target matrix of shape 1 x n
     """
 
-    def __init__(self, d):
-        self.d = d
-        self.w = None
-
+    def __init__(self, d, is_adaline=False):
+        self.d, self.w, self.is_adaline = d, None, is_adaline
         self.init_w()
-    
+
     def init_w(self):
         np.random.seed(999)
         self.w = np.random.rand(1, self.d)
 
-    def predict(self, x):
+    def predict_train(self, x):
+        if not self.is_adaline:
+            y_hat = np.sign(self.w @ x)
+            y_hat[y_hat == 0] = 1
+            return y_hat
+        else:
+            return self.w @ x
+
+    def predict_test(self, x):
         y_hat = np.sign(self.w @ x)
         y_hat[y_hat == 0] = 1
         return y_hat
 
     def get_accuracy(self, x, y):
-        return np.mean(self.predict(x) == y)
+        return np.mean(self.predict_test(x) == y)
+
+    def get_loss(self, x, y):
+        return np.mean((self.predict_train(x) - y) ** 2)
 
     def train(self, x, y, pca, epochs=10, lr=0.1, plot_interval=1, plot_progress=True):
         """
@@ -36,29 +45,40 @@ class Perceptron:
 
         self.init_w()
 
-        to_plot = []
+        plot_acc, plot_loss = [], []
         tot_epochs = 0
 
         for e in range(epochs):
             # Train
             for i in range(x.shape[1]):
-                x_i, y_i = x[:, i:i + 1], y[:, i:i + 1]
-                y_hat_i = self.predict(x_i)
+                x_i, y_i = x[:, i : i + 1], y[:, i : i + 1]
+                y_hat_i = self.predict_train(x_i)
+                # print(x_i, y_i, y_hat_i)
                 if y_hat_i != y_i:
-                    self.w += lr * (-y_hat_i * x_i.T)
+                    if not self.is_adaline:
+                        self.w += lr * (-y_hat_i * x_i.T)
+                    else:
+                        self.w += lr * (-y_hat_i + y_i) * x_i.T
 
                 if i % plot_interval == 0:
-                    to_plot.append(self.get_accuracy(x, y))
+                    plot_acc.append(self.get_accuracy(x, y))
+                    plot_loss.append(self.get_loss(x, y))
 
             tot_epochs += 1
             if self.get_accuracy(x, y) == 1:
                 break
 
         if plot_progress:
-            plt.plot(to_plot)
+            plt.plot(plot_acc)
             plt.xlabel("Training step (per example)")
             plt.ylabel("Accuracy")
             plt.title("Perceptron training accuracy over time")
+            plt.show()
+
+            plt.plot(plot_loss)
+            plt.xlabel("Training step (per example)")
+            plt.ylabel("MSE")
+            plt.title("Perceptron training loss over time")
             plt.show()
 
         return tot_epochs
